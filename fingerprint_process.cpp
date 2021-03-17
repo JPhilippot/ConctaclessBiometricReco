a8a98096db02e14c2e09b54a2896461a8b963937
beoffre <felix.yriarte@gmail.com>
2021-03-17 09:52:42 +0100

    essais avec CN

--- fingerprint_process.cpp
+++ fingerprint_process.cpp
@@ -40,6 +40,31 @@ void thinningIteration(Mat& im, int iter)
     im &= ~marker;
 }
 
+void createCrossingNumber(Mat& im, Mat& out){
+    Mat marker = Mat::zeros(im.size(), CV_8UC1 );
+    for (int i=1;i<im.rows-1;i++){
+        for (int j=1; j<im.cols-1 ; j++){
+            uchar p2 = im.at<uchar>(i-1, j);
+            uchar p3 = im.at<uchar>(i-1, j+1);
+            uchar p4 = im.at<uchar>(i, j+1);
+            uchar p5 = im.at<uchar>(i+1, j+1);
+            uchar p6 = im.at<uchar>(i+1, j);
+            uchar p7 = im.at<uchar>(i+1, j-1);
+            uchar p8 = im.at<uchar>(i, j-1);
+            uchar p9 = im.at<uchar>(i-1, j-1);
+
+            if (im.at<uchar>(i,j)==1){
+                marker.at<uchar>(i,j)=(abs(p2-p3)+abs(p3-p4)+abs(p4-p5)+abs(p5-p6)+abs(p6-p7)+abs(p7-p8)+abs(p8-p9)+abs(p9-p2))/2;
+            }
+            else{
+                marker.at<uchar>(i,j)=0;
+            }
+        }
+    }
+    out=marker;
+
+}
+
 // Function for thinning any given binary image within the range of 0-255. If not you should first make sure that your image has this range preset and configured!
 void thinning(Mat& im)
 {
@@ -90,46 +115,79 @@ int main( int argc, const char** argv )
 
     // Now lets detect the strong minutiae using Haris corner detection
     Mat harris_corners, harris_normalised;
-    harris_corners = Mat::zeros(input_thinned.size(), CV_32FC1);
-    cornerHarris(input_thinned, harris_corners, 2, 3, 0.04, BORDER_DEFAULT);
+    harris_corners = Mat::zeros(input_thinned.size(), CV_8UC1);
+
+
+    //cornerHarris(input_thinned, harris_corners, 2, 3, 0.04, BORDER_DEFAULT);
+
+    //normalize(input_thinned, input_thinned, 0, 1, NORM_MINMAX, CV_8UC1, Mat());
+    
+
+
+    input_thinned.copyTo( container( Rect(0, 0, input.cols, input.rows) ) );
+    harris_corners.copyTo(container(Rect(input.cols,0,input.cols,input.rows)));
+
+    imshow("thin vs CN", container) ; waitKey(0);
+
+
+    Mat crossingNumber;
+
+    threshold(input_thinned, crossingNumber, 0, 1, THRESH_BINARY | THRESH_OTSU);
+
+    createCrossingNumber(crossingNumber, harris_corners);
+
+    vector<KeyPoint> keypoints;
+
+    for(int x=0; x<harris_corners.cols; x++){
+        for(int y=0; y<harris_corners.rows; y++){
+            if ((int)harris_corners.at<int>(y,x)==1 || (int)harris_corners.at<int>(y,x)==3){
+                circle(harris_corners, Point(x, y), 5, Scalar(0,255,0), 1);
+                circle(harris_corners, Point(x, y), 1, Scalar(0,0,255), 1);
+                keypoints.push_back( KeyPoint (x, y, 1) );
+            }
+        }
+    }
+
+
     //Sobel(input_thinned, harris_corners, 0, 2, 2);
-    normalize(harris_corners, harris_normalised, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+    //normalize(harris_corners, harris_normalised, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
 
     // Select the strongest corners that you want
     int threshold_harris = 125;
-    vector<KeyPoint> keypoints;
+    
 
     // Make a color clone for visualisation purposes
     Mat rescaled;
 
-    harris_normalised.copyTo(container (Rect(0,0,input.cols, input.rows)));
+    //harris_normalised.copyTo(container (Rect(0,0,input.cols, input.rows)));
+    input_thinned.copyTo( container( Rect(0, 0, input.cols, input.rows) ) );
     harris_corners.copyTo(container(Rect(input.cols,0,input.cols,input.rows)));
 
-    imshow("normalized -> toCompare with threshold", container) ; waitKey(0);
+    imshow("thin vs CN 1 and 3", container) ; waitKey(0);
 
-    convertScaleAbs(harris_normalised, rescaled);
-    Mat harris_c(rescaled.rows, rescaled.cols, CV_8UC3);
-    Mat in[] = { rescaled, rescaled, rescaled };
-    int from_to[] = { 0,0, 1,1, 2,2 };
-    mixChannels( in, 3, &harris_c, 1, from_to, 3 );
-    for(int x=0; x<harris_normalised.cols; x++){
-       for(int y=0; y<harris_normalised.rows; y++){
-          if ( (int)harris_normalised.at<float>(y, x) > threshold_harris ){
-             // Draw or store the keypoint location here, just like you decide. In our case we will store the location of the keypoint
-             circle(harris_c, Point(x, y), 5, Scalar(0,255,0), 1);
-             circle(harris_c, Point(x, y), 1, Scalar(0,0,255), 1);
-             keypoints.push_back( KeyPoint (x, y, 1) );
-          }
-       }
-    }
-    imshow("temp", harris_c); waitKey(0);
+    // convertScaleAbs(harris_normalised, rescaled);
+    // Mat harris_c(rescaled.rows, rescaled.cols, CV_8UC3);
+    // Mat in[] = { rescaled, rescaled, rescaled };
+    // int from_to[] = { 0,0, 1,1, 2,2 };
+    // mixChannels( in, 3, &harris_c, 1, from_to, 3 );
+    // for(int x=0; x<harris_normalised.cols; x++){
+    //    for(int y=0; y<harris_normalised.rows; y++){
+    //       //if ( (int)harris_normalised.at<float>(y, x) > threshold_harris ){
+    //          // Draw or store the keypoint location here, just like you decide. In our case we will store the location of the keypoint
+    //          circle(harris_c, Point(x, y), 5, Scalar(0,255,0), 1);
+    //          circle(harris_c, Point(x, y), 1, Scalar(0,0,255), 1);
+    //          keypoints.push_back( KeyPoint (x, y, 1) );
+    //       }
+    //    }
+    // }
+    // imshow("temp", harris_c); waitKey(0);
 
     // Compare both
     Mat ccontainer(input.rows, input.cols*2, CV_8UC3);
     Mat input_thinned_c = input_thinned.clone();
     cvtColor(input_thinned_c, input_thinned_c, COLOR_GRAY2BGR);
     input_thinned_c.copyTo( ccontainer( Rect(0, 0, input.cols, input.rows) ) );
-    harris_c.copyTo( ccontainer( Rect(input.cols, 0, input.cols, input.rows) ) );
+    //harris_c.copyTo( ccontainer( Rect(input.cols, 0, input.cols, input.rows) ) );
     imshow("thinned versus selected corners", ccontainer); waitKey(0);
 
     // Calculate the ORB descriptor based on the keypoint
