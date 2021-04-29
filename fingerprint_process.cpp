10e92dae4fb85d0c9c45a55199931573f80f80fc
beoffre <felix.yriarte@gmail.com>
2021-04-29 15:04:20 +0200

    essais kmeans/centroids (repair kmeans en cours)

--- fingerprint_process.cpp
+++ fingerprint_process.cpp
@@ -176,6 +176,129 @@ double compareWithHaris(char* nom1, char* nom2, int harrisThreshold, double radi
 
 }
 
+float distance(float x1, float y1, float x2, float y2){
+    return std::sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+float distance(Point2f p1, float x2, float y2){
+    return std::sqrt((p1.x-x2)*(p1.x-x2)+(p1.y-y2)*(p1.y-y2));
+}
+float distance(Point2f p1, Point2f p2){
+    return std::sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+}
+
+std::vector<KeyPoint>* centroidPicking(std::vector<KeyPoint> &listePoints, int nombreMinutiaeFinal){
+
+
+    float centroidX =0.0;                           
+    float centroidY =0.0;
+
+    for (int k=0;k<(int)listePoints.size();k++){   
+        centroidX+=listePoints[k].pt.x;
+        centroidY+=listePoints[k].pt.y;
+    }
+    centroidX /=(float)listePoints.size();
+    centroidY/=(float)listePoints.size();
+    
+
+    //cherrypick centered minutiae (ugly sorting algorithm ?)
+
+    std::vector<KeyPoint>* res = new std::vector<KeyPoint>();
+    for(int c =0; c<std::min(nombreMinutiaeFinal,(int)listePoints.size());c++){
+        float minDist = distance(listePoints[0].pt, centroidX,centroidY);
+        int minK=0;
+        for (int k=0;k<listePoints.size();k++){
+            if (distance(listePoints[k].pt, centroidX,centroidY)<minDist){
+                minDist=distance(listePoints[k].pt, centroidX,centroidY);
+                minK=k;
+            }
+        }
+        res->push_back(listePoints[minK]);
+        listePoints.erase(listePoints.begin()+minK);
+    }
+
+    return (res);
+
+
+}
+
+std::vector<KeyPoint>* KMeansClusterization(std::vector<KeyPoint>& minutiae, int nombreMinutiaeFinal){
+
+    std::vector<KeyPoint>* centroidArray = new std::vector<KeyPoint>();
+
+    std::vector<Point2f> ClusterCenters = *new std::vector<Point2f>();
+
+// on stocke ici les clusters .... useless ?
+    // std::vector<std::vector<Point2f>> clusteredMinutiae = *new std::vector<std::vector<Point2f>>();
+    // for(int i=0;i<nombreMinutiaeFinal;i++){
+    //     clusteredMinutiae[i]= *new std::vector<Point2f>();
+    // }
+
+
+    int randIndex = (int)(rand()%minutiae.size());
+    KeyPoint c1 = minutiae[0];
+    centroidArray->push_back(c1);
+
+    // float meanDist = 0.0;
+    // for (int k=0; k<(int)minutiae.size();k++){
+    //     meanDist+=distance(c1.pt,minutiae[k].pt);
+    // }
+    // meanDist/=(float)minutiae.size();
+
+
+    //KeyPoint c2;
+
+//comment recuperer un point "assez distant" ? peut etre prendre plus loin ...
+    // for(int k=0;k<(int)minutiae.size();k++){
+    //     if (k!=randIndex){
+    //         if (distance(c1.pt, minutiae[k].pt)/meanDist>2.0 || distance(c1.pt, minutiae[k].pt)/meanDist<0.5){
+    //             c2=minutiae[k];
+    //             //printf("c'est bon j'ai trouve");
+    //             break;
+    //         }
+    //     }
+    // }
+    //centroidArray->push_back(c2);
+
+//premiere clusterisation
+    // for(int k=0;k<(int)minutiae.size();k++){
+    //     if (distance(minutiae[k].pt,centroidArray[0])<distance(minutiae[k].pt,centroidArray[1])){
+    //         clusteredMinutiae[0].push_back(minutiae[k].pt);
+    //     }
+    //     else{clusteredMinutiae[1].push_back(minutiae[k].pt);}
+    // }
+
+
+//construction de centroids de clusters au fur et a mesure //ET remplissage tabClusters
+    for (int c=1;c<nombreMinutiaeFinal;c++){
+
+    //trouver le point le plus distant => nouveau centre de cluster
+        float furthestDist = 0.0;
+        int furthestPointIndice=0;
+        for (int k =0;k<(int)(minutiae.size());k++){
+            float dist = 0.0;
+            for (int center=0;center<=c;center++){
+                dist+=distance(minutiae[k].pt,(*centroidArray)[center].pt);
+            }
+            if (dist>furthestDist){
+                    furthestDist = dist;
+                    furthestPointIndice=k;
+                }
+        }
+    //furthest point has been found. must now add it to clusterCentroids
+        centroidArray->push_back(minutiae[furthestPointIndice]);
+    }
+    //printf("%d taille\n",(int)centroidArray->size());
+
+    // for (int k=0;k<(int)centroidArray->size();k++){
+    //     printf("x : %f     y : %f\n",(*centroidArray)[k].pt.x,(*centroidArray)[k].pt.y);
+    // }
+
+    return centroidArray;
+    
+
+}
+
+
 void compareWithCN(string nom1, string nom2, double radiusSize, bool samePerson, int* VP, int* FP, int*VN, int* FN){
 
     Mat input = imread(nom1, IMREAD_GRAYSCALE);
@@ -199,18 +322,42 @@ void compareWithCN(string nom1, string nom2, double radiusSize, bool samePerson,
     Mat in[] = { rescaled, rescaled, rescaled };
     int from_to[] = { 0,0, 1,1, 2,2 };
     mixChannels( in, 3, &harris_c, 1, from_to, 3 );
+
+
+    // Mat container(input.rows, input.cols*2, CV_8UC1);
+    // input.copyTo( container( Rect(0, 0, input.cols, input.rows) ) );
+    // input_binary.copyTo( container( Rect(input.cols, 0, input.cols, input.rows) ) );
+    
+
+
     for(int x=0; x<harris_corners.cols; x++){
         for(int y=0; y<harris_corners.rows; y++){
-            if ((int)harris_corners.at<uchar>(y,x)==3 || (int)harris_corners.at<uchar>(y,x)==1){
-                circle(harris_c, Point(x, y), 5, Scalar(0,255,0), 1);
-                circle(harris_c, Point(x, y), 1, Scalar(0,0,255), 1);
+            if ((int)harris_corners.at<uchar>(y,x)==3 || (int)harris_corners.at<uchar>(y,x)==3){
+                //circle(harris_c, Point(x, y), 5, Scalar(0,255,0), 1);
+                circle(harris_c, Point(x, y), 2, Scalar(0,0,255), 1);
                 keypoints.push_back( KeyPoint (x, y, 1) );
             }
         }
     }
+    
+
+    //compute the barycentre 
+
+    std::vector<KeyPoint> cherrypicked = *centroidPicking(keypoints, 20);
+
+    //OR use kmeans clusters :
+
+    //std::vector<KeyPoint> cherrypicked = *KMeansClusterization(keypoints, 20);
+    for(int pick =0;pick<cherrypicked.size();pick++){
+        circle(harris_c, Point(cherrypicked[pick].pt.x, cherrypicked[pick].pt.y), 5, Scalar(0,255,0), 1);
+    }
+        
+    imshow("input versus binary", harris_c); waitKey(0);
+
+
     Ptr<Feature2D> orb_descriptor = ORB::create();
     Mat descriptors;
-    orb_descriptor->compute(input_thinned, keypoints, descriptors);
+    orb_descriptor->compute(input_thinned, cherrypicked, descriptors); //cherrypicked instead
 
 
 
@@ -228,22 +375,34 @@ void compareWithCN(string nom1, string nom2, double radiusSize, bool samePerson,
     normalize(harris_corners2, harris_normalised2, 0, 255, NORM_MINMAX, CV_8UC1, Mat());
     vector<KeyPoint> keypoints2;
     Mat rescaled2;
-    convertScaleAbs(harris_normalised, rescaled);
-    Mat harris_c2(rescaled.rows, rescaled.cols, CV_8UC3);
-    Mat in2[] = { rescaled, rescaled, rescaled };
+    convertScaleAbs(harris_normalised2, rescaled2);
+    Mat harris_c2(rescaled2.rows, rescaled2.cols, CV_8UC3);
+    Mat in2[] = { rescaled2, rescaled2, rescaled2 };
     int from_to2[] = { 0,0, 1,1, 2,2 };
-    mixChannels( in, 3, &harris_c, 1, from_to, 3 );
+    mixChannels( in2, 3, &harris_c2, 1, from_to2, 3 );
     for(int x=0; x<harris_corners2.cols; x++){
         for(int y=0; y<harris_corners2.rows; y++){
             if ((int)harris_corners2.at<uchar>(y,x)==3 || (int)harris_corners2.at<uchar>(y,x)==3){
-                circle(harris_c2, Point(x, y), 5, Scalar(0,255,0), 1);
-                circle(harris_c2, Point(x, y), 1, Scalar(0,0,255), 1);
+                //circle(harris_c2, Point(x, y), 5, Scalar(0,255,0), 1);
+                circle(harris_c2, Point(x, y), 2, Scalar(0,0,255), 1);
                 keypoints2.push_back( KeyPoint (x, y, 1) );
             }
         }
     }
+
+
+    //std::vector<KeyPoint> cherrypicked2 = *centroidPicking(keypoints2, 20);
+    std::vector<KeyPoint> cherrypicked2 = *KMeansClusterization(keypoints2, 20);
+
+    for(int pick =0;pick<cherrypicked2.size();pick++){
+        circle(harris_c2, Point(cherrypicked2[pick].pt.x, cherrypicked2[pick].pt.y), 5, Scalar(0,255,0), 1);
+    }
+        
+    imshow("input versus binary", harris_c2); waitKey(0);
+
+
     Mat descriptors2;
-    orb_descriptor->compute(input_thinned2, keypoints2, descriptors2);
+    orb_descriptor->compute(input_thinned2, cherrypicked2, descriptors2);
 
     //printf("problemeavant\n");
     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
@@ -307,24 +466,25 @@ int main( int argc, const char** argv )
         int *VN=new int; *VN=0;
         int *FN=new int; *FN=0;
         
-        for (int personne = 1;personne<10;personne++){
-            for (int image =1;image<5;image++){
-                if (!(personne==9 && image==1)){
-                    std::string deb = std::string("cleaned/10");
-                    deb += std::to_string(personne);
-                    deb += std::string("_");
-                    deb+=std::to_string(image);
-                    deb+=std::string("_cleaned.tif");
-                    compareWithCN(string("cleaned/109_1_cleaned.tif"),deb,(double)i,personne==9, VP, FP, VN, FN);
-                }
-            }
-        }
+        //for (int personne = 1;personne<10;personne++){
+            //for (int image =1;image<5;image++){
+                //if (!(personne==9 && image==1)){
+                    // std::string deb = std::string("cleaned/10");
+                    // deb += std::to_string(personne);
+                    // deb += std::string("_");
+                    // deb+=std::to_string(image);
+                    // deb+=std::string("_cleaned.tif");
+                    compareWithCN(string("cleaned/106_8_cleaned.tif"),string("cleaned/102_1_cleaned.tif"),(double)i,!true, VP, FP, VN, FN);
+                    printf("VP : %d FP : %d VN : %d FN : %d\n",*VP,*FP,*VN,*FN);
+                //}
+            //}
+        //}
 
         
         double sensitivite = (double)((double)*VP/(double)(*VP+*FN));
         double specificite = 1.0-(double)((double)*VN/(double)(*VN+*FP));
-        minDist=std::min(minDist,sqrt(pow(0.0-sensitivite,2.0)+pow(1.0-specificite,2.0)));
-        printf("%d %f %f %f\n",i,sensitivite,specificite,sqrt(pow(0.0-sensitivite,2.0)+pow(1.0-specificite,2.0)));
+        //minDist=std::min(minDist,sqrt(pow(0.0-sensitivite,2.0)+pow(1.0-specificite,2.0)));
+        //printf("%d %f %f %f\n",i,sensitivite,specificite,sqrt(pow(0.0-sensitivite,2.0)+pow(1.0-specificite,2.0)));
         
         
     }
